d5baad7cd70478e84928a8ff17f1d6a6d3599279
rai <gabriel2012688@googlemail.com>
2019-07-27 15:22:36 +0200

    add features yay

--- randfuncgen.py
+++ randfuncgen.py
@@ -5,40 +5,50 @@ from random import choice
 
 parser = argparse.ArgumentParser(description = 'ep1c p4st1ng t00l')
 parser.add_argument('num', help='amount of lines', type=int)
+parser.add_argument('--digitnum', default='6', help='the amount of digits for the int\'s values', dest='digitnum', type=int)
+parser.add_argument('--namelen', default='12', help='length of the random generated variable names and strings', dest='namelen', type=int)
+parser.add_argument('--strlen', default='16', help ='length of the random generated variable content (strings)', dest='strlen', type=int)
+parser.add_argument('--funccount', default='1', help='amount of functions to generate', dest='funccount', type=int)
 args = parser.parse_args()
 
 chars = string.ascii_uppercase + string.ascii_lowercase
 numbers = "123456789"
 
 def makename():
-	return "".join(choice(chars) for i in range(0,12))
+	return "".join(choice(chars) for i in range(0,args.namelen))
+
+def strcont():
+	return "".join(choice(chars) for i in range(0,args.strlen))
 
 def main():
+	x = 0
 	with open('out.cpp', '+w') as file:
-		file.write('#include <string>\n#include <iostream>\n#include <stdio.h>\n\nusing namespace std;\n')
-		file.write(f'void {makename()}()' + '{\n')
-		i = 0
-		while i < args.num:
-			type = choice(['b', 's', 'i', 'if'])
-			if type == "b":
-				boolstat = choice(['true', 'false'])
-				file.write(f'    bool {makename()} = {boolstat};' + '\n')
-			if type == "s":
-				cont = choice(['true', 'false'])
-				if cont == 'true':
-					file.write(f'    string {makename()} = "{makename()}";' + '\n')
-				else:
-					file.write(f'    string {makename()};' + '\n')
-			if type == "i":
-				digits = "".join(choice(numbers) for i in range(0,6))
-				file.write(f'    int {makename()} = {digits};' + '\n')
-			if type == "if":
-				epicname = makename()
-				boolstat = choice(['true', 'false'])
-				file.write(f'    bool {epicname} = {boolstat};' + '\n')
-				file.write(f'    if({epicname} == true)' + '{' + f'{epicname} = false;' + '}\n')
-			i = i + 1
-		file.write('}')
+		file.write('#include <string>\n#include <iostream>\n#include <stdio.h>\n\n')
+		while x <= args.funccount:
+			file.write(f'void {makename()}()' + '{\n')
+			i = 0
+			while i <= args.num:
+				type = choice(['b', 's', 'i', 'if'])
+				if type == "b":
+					boolstat = choice(['true', 'false'])
+					file.write(f'    bool {makename()} = {boolstat};' + '\n')
+				if type == "s":
+					cont = choice(['true', 'false'])
+					if cont == 'true':
+						file.write(f'    string {makename()} = "{strcont()}";' + '\n')
+					else:
+						file.write(f'    string {makename()};' + '\n')
+				if type == "i":
+					digits = "".join(choice(numbers) for i in range(0,args.digitnum))
+					file.write(f'    int {makename()} = {digits};' + '\n')
+				if type == "if":
+					epicname = makename()
+					boolstat = choice(['true', 'false'])
+					file.write(f'    bool {epicname} = {boolstat};' + '\n')
+					file.write(f'    if({epicname} == true)' + '{' + f'{epicname} = false;' + '}\n')
+				i += 1
+			x += 1
+			file.write('}\n\n')
 		print('done. file is in the directory you ran the program in.')
 
 if __name__ == '__main__':
